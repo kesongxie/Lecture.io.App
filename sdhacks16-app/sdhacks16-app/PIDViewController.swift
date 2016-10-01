9ec6569f8f44cc53487d63c34da5db4e5a46a58c
Benjamin Nagengast <bennorthn@gmail.com>
2016-10-01 01:00:09 -0700

    PID VC

--- sdhacks16-app/sdhacks16-app/PIDViewController.swift
+++ sdhacks16-app/sdhacks16-app/PIDViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PIDViewController: UIViewController {
 
+	@IBOutlet weak var PIDTextView: UITextField!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
 	override func didReceiveMemoryWarning() {
