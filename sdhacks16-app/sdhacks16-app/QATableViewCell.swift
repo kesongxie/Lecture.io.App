3574f89bc8ef09055a48228b1baea360ef71171c
Benjamin Nagengast <bennorthn@gmail.com>
2016-10-01 12:06:04 -0700

    QAView controller formatted properly

---
+++ sdhacks16-app/sdhacks16-app/QATableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  QATableViewCell.swift
+//  sdhacks16-app
+//
+//  Created by Benjamin Nagengast on 10/1/16.
+//  Copyright © 2016 sdhacks16. All rights reserved.
+//
+
+import UIKit
+
+class QATableViewCell: UITableViewCell {
+	
+	@IBOutlet weak var answerTextLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- sdhacks16-app/sdhacks16-app/QAViewController.swift
+++ sdhacks16-app/sdhacks16-app/QAViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class QAViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-	var tableView = UITableView()
-	var questionIDTemp = UITextField()
-	var sendButton = UIButton()
 	
+	@IBOutlet weak var questionID: UITextField!
+	@IBOutlet weak var sendButton: UIButton!
+	@IBOutlet weak var tableView: UITableView!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
 		self.tableView.delegate = self
-		
+		self.tableView.dataSource = self
+		//self.tableView.tableFooterView = UIView()
+		//self.tableView
         // Do any additional setup after loading the view.
     }
 
@@ -30,37 +32,49 @@ class QAViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+		self.view.endEditing(true)
+	}
+	func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+		tableView.cellForRow(at: indexPath)?.accessoryType = .none
+		return indexPath
+	}
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
 	}
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "QATableViewCell", for: indexPath) as! QATableViewCell
 		
 		switch indexPath.row {
 		case 0:
-			cell.textLabel = 'A'
+			cell.answerTextLabel.text = "A"
 		case 1:
-			cell.textLabel = 'B'
+			cell.answerTextLabel.text = "B"
 		case 2:
-			cell.textLabel = 'C'
+			cell.answerTextLabel.text = "C"
 		case 3:
-			cell.textLabel = 'D'
+			cell.answerTextLabel.text = "D"
+		case 4:
+			cell.answerTextLabel.text = "E"
+		default:
+			cell.answerTextLabel.text = ""
 		}
 		return cell
 	}
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 4
+		return 5
 	}
 	func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .none
 	}
 	
 	func sendButtonPressed() {
-		let selectedCell = self.tableView.cellForRow(at: self.tableView.indexPathForSelectedRow)
-		if let pid = UserDefaults.standard.string(forKey: "pidValue"),
-	 			let questionID = questionIDTemp.text,
-	 			let responseString = selectedCell.text {
+		if let indexPath = self.tableView.indexPathForSelectedRow {
+			let qid = questionID.text
+			let selectedCell = self.tableView.cellForRow(at: indexPath) as! QATableViewCell
+			let responseString = selectedCell.answerTextLabel.text
+			let pid = UserDefaults.standard.string(forKey: "pidValue")
 	 			let parameters: Parameters = [
 	 				"pid" : pid,
-	 				"qid" : questionID,
+	 				"qid" : qid,
 	 				"response": responseString
 			  ]
 				Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
@@ -68,9 +82,7 @@ class QAViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
 	 				print("Response: \(response.response)")
 	 				print("Error: \(response.data)")
 	 			}
-	 			return true
 	 		}
-		return false
 	}
 
     /*
